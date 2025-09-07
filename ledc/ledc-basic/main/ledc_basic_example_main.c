0b7b8f15ee56b29b1b72d67700f28a628397c556
Dragos Hatagan <dragos.hategan@yahoo.com>
2025-09-07 12:27:30 +0300

    Update ledc_basic_example_main.c
    Added a second led configuration.

--- ledc/ledc-basic/main/ledc_basic_example_main.c
+++ ledc/ledc-basic/main/ledc_basic_example_main.c
@@ -10,13 +10,21 @@
 #include "driver/ledc.h"
 #include "esp_err.h"
 
-#define LEDC_TIMER              LEDC_TIMER_0
-#define LEDC_MODE               LEDC_LOW_SPEED_MODE
-#define LEDC_OUTPUT_IO          (5) // Define the output GPIO
-#define LEDC_CHANNEL            LEDC_CHANNEL_0
-#define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
-#define LEDC_DUTY               (4096) // Set duty to 50%. (2 ** 13) * 50% = 4096
-#define LEDC_FREQUENCY          (4000) // Frequency in Hertz. Set frequency at 4 kHz
+#define LEDC1_TIMER              LEDC_TIMER_0
+#define LEDC1_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC1_OUTPUT_IO          (32) // Define the output GPIO
+#define LEDC1_CHANNEL            LEDC_CHANNEL_0
+#define LEDC1_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC1_DUTY               (4096) // Set duty to 50%. (2 ** 13) * 50% = 4096
+#define LEDC1_FREQUENCY          (4000) // Frequency in Hertz. Set frequency at 4 kHz
+
+#define LEDC2_TIMER              LEDC_TIMER_0
+#define LEDC2_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC2_OUTPUT_IO          (14) // Define the output GPIO
+#define LEDC2_CHANNEL            LEDC_CHANNEL_1
+#define LEDC2_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC2_DUTY               (8192) // Set duty to 50%. (2 ** 13) * 50% = 4096
+#define LEDC2_FREQUENCY          (4000) // Frequency in Hertz. Set frequency at 4 kHz
 
 /* Warning:
  * For ESP32, ESP32S2, ESP32S3, ESP32C3, ESP32C2, ESP32C6, ESP32H2 (rev < 1.2), ESP32P4 targets,
@@ -24,29 +32,60 @@
  * 100% duty cycle is not reachable (duty cannot be set to (2 ** SOC_LEDC_TIMER_BIT_WIDTH)).
  */
 
-static void example_ledc_init(void)
+static void init_led_1(void)
 {
     // Prepare and then apply the LEDC PWM timer configuration
     ledc_timer_config_t ledc_timer = {
-        .speed_mode       = LEDC_MODE,
-        .duty_resolution  = LEDC_DUTY_RES,
-        .timer_num        = LEDC_TIMER,
-        .freq_hz          = LEDC_FREQUENCY,  // Set output frequency at 4 kHz
+        .speed_mode       = LEDC1_MODE,
+        .duty_resolution  = LEDC1_DUTY_RES,
+        .timer_num        = LEDC1_TIMER,
+        .freq_hz          = LEDC1_FREQUENCY,  // Set output frequency at 4 kHz
         .clk_cfg          = LEDC_AUTO_CLK
     };
     ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
-
+    
     // Prepare and then apply the LEDC PWM channel configuration
     ledc_channel_config_t ledc_channel = {
-        .speed_mode     = LEDC_MODE,
-        .channel        = LEDC_CHANNEL,
-        .timer_sel      = LEDC_TIMER,
+        .speed_mode     = LEDC1_MODE,
+        .channel        = LEDC1_CHANNEL,
+        .timer_sel      = LEDC1_TIMER,
         .intr_type      = LEDC_INTR_DISABLE,
-        .gpio_num       = LEDC_OUTPUT_IO,
+        .gpio_num       = LEDC1_OUTPUT_IO,
         .duty           = 0, // Set duty to 0%
         .hpoint         = 0
     };
     ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+}
+
+static void init_led_2(void)
+{
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode       = LEDC2_MODE,
+        .duty_resolution  = LEDC2_DUTY_RES,
+        .timer_num        = LEDC2_TIMER,
+        .freq_hz          = LEDC2_FREQUENCY,  // Set output frequency at 4 kHz
+        .clk_cfg          = LEDC_AUTO_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+    
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode     = LEDC2_MODE,
+        .channel        = LEDC2_CHANNEL,
+        .timer_sel      = LEDC2_TIMER,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .gpio_num       = LEDC2_OUTPUT_IO,
+        .duty           = 0, // Set duty to 0%
+        .hpoint         = 0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+}
+
+static void example_ledc_init(void)
+{
+    init_led_1();
+    init_led_2();
 }
 
 void app_main(void)
@@ -54,7 +93,12 @@ void app_main(void)
     // Set the LEDC peripheral configuration
     example_ledc_init();
     // Set duty to 50%
-    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
+    ESP_ERROR_CHECK(ledc_set_duty(LEDC1_MODE, LEDC1_CHANNEL, LEDC1_DUTY));
     // Update duty to apply the new value
-    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC1_MODE, LEDC1_CHANNEL));
+
+    // Set duty to 50%
+    ESP_ERROR_CHECK(ledc_set_duty(LEDC2_MODE, LEDC2_CHANNEL, LEDC2_DUTY));
+    // Update duty to apply the new value
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC2_MODE, LEDC2_CHANNEL));
 }
